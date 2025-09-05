60877e6ca7d8001a6e5f68545dbd3d0561a3f63c
remykav <rkavanagh1@hwemail.com>
2025-09-05 09:20:09 -0700

    rectangle

---
+++ Assignments/Unit 0 Rectangle Lab/Rectangle.java
@@ -0,0 +1,24 @@
+public class Rectangle {
+
+    // instance variables
+    private int length;
+    private int width;
+
+    // constructors
+    public Rectangle(int length, int width){
+          length = input length;
+        width = input width;
+    }
+
+    // getters
+    public int getLength(){
+        return int length;
+    }
+
+    public int getwidth(){
+        return int width;
+    }
+
+
+
+}
